1c5e5d4b6355e191c2292bd42c68738bb3c0b1ca
jayanshukikani-1412 <jayanshukikani14@gmail.com>
2026-03-06 19:35:30 +0530

    Refactor app.js to comment out all request handlers and retain only the server listening functionality

--- src/app.js
+++ src/app.js
@@ -4,45 +4,48 @@ const express = require("express");
 // Instance of express application
 const app = express();
 
-// Hello Request Handler
-app.use("/api/hello", (req, res) => {
-  res.send("Hello From Server");
+// Listening to the port 8080
+app.listen(8080, () => {
+  console.log("Server is successfully running on port 8080");
 });
+
+
+
+
+
+// Hello Request Handler
+// app.use("/api/hello", (req, res) => {
+//   res.send("Hello From Server");
+// });
 
 // Test Request Handler
-app.use("/api/test", (req, res) => {
-  res.send("Test From Server");
-});
+// app.use("/api/test", (req, res) => {
+//   res.send("Test From Server");
+// });
 
-app.get("/api/users", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@example.com",
-    },
-  ]);
-});
+// app.get("/api/users", (req, res) => {
+//   res.send([
+//     {
+//       id: 1,
+//       name: "John Doe",
+//       email: "john.doe@example.com",
+//     },
+//   ]);
+// });
 
-app.post("/api/users", (req, res) => {
-  res.send("User created successfully");
-});
+// app.post("/api/users", (req, res) => {
+//   res.send("User created successfully");
+// });
 
-app.put("/api/users/:id", (req, res) => {
-  res.send("User updated successfully");
-});
+// app.put("/api/users/:id", (req, res) => {
+//   res.send("User updated successfully");
+// });
 
-app.delete("/api/users/:id", (req, res) => {
-  res.send("User deleted successfully");
-});
+// app.delete("/api/users/:id", (req, res) => {
+//   res.send("User deleted successfully");
+// });
 
 
 // app.use("/", (req, res) => {
 //   res.send("Namaste From Server");
 // });
-
-
-// Listening to the port 8080
-app.listen(8080, () => {
-  console.log("Server is successfully running on port 8080");
-});
