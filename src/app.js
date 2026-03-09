c2464d6d9bd9ad1a8db2ae563c278539f6e14ad8
jayanshukikani-1412 <jayanshukikani14@gmail.com>
2026-03-09 19:57:05 +0530

    Add user management endpoints for retrieving, updating, and deleting users in app.js. Enhance error handling for user operations and ensure consistent response messages.

--- src/app.js
+++ src/app.js
@@ -3,26 +3,81 @@ const express = require("express");
 const connectDB = require("./config/database");
 // Instance of express application
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
 
 app.use(express.json());
- 
-app.post("/signup", async (req, res) => {
-console.log("req", req.body)
-  const user = new User(req.body)
 
+// Signup API ==>> POST /signup ==>> for signup new user
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   try {
-    await user.save()
-    res.send("User added successfully")
+    await user.save();
+    res.send("User added successfully");
   } catch (error) {
-    console.log("Error", error)
-    res.status(400).send("Error saving the user: ", error.message)
+    console.log("Error", error);
+    res.status(400).send("Error saving the user: ", error.message);
   }
-})
+});
+
+// Find User By Email ==>> GET /user ==>> find one user from email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// User Feed API ==>> GET /feed ==>> get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 connectDB()
   .then(() => {
-    console.log("Database connected successfully")
+    console.log("Database connected successfully");
     // Listening to the port 8080
     app.listen(8080, () => {
       console.log("Server is successfully running on port 8080");
